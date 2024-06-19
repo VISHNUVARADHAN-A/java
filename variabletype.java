211d6e8abc5baba4a8987a935aab92dd64080640
VISHNUVARADHAN-A <vishnuviva396@gmail.com>
2024-06-19 23:18:16 +0530

    vartype

---
+++ variabletype.java
@@ -0,0 +1,19 @@
+public class variabletype {
+    public static void main(String[] args) {
+        
+        int MyNumber = 5;
+        System.out.println("this is number " + MyNumber);
+        String MyString ="hellow world";
+        System.out.println("this is String " + MyString);
+        float MyFloat = 9.5f;
+        System.err.println("this is float " + MyFloat);
+        Double MyDouble= 9.5;
+        System.out.println("this is Double " + MyDouble);
+        boolean MyBoolen = true;
+        System.out.println("this is boolen " + MyBoolen);
+        char MyChar ='a';
+        System.out.println("this is character " + MyChar);
+
+
+    }
+}
