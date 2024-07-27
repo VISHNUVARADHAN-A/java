a3ea1037f96cf83d27d90ca6326e38396b90206d
VISHNUVARADHAN-A <vishnuviva396@gmail.com>
2024-07-28 03:12:03 +0530

    initial

---
+++ diffrence_in_print.java
@@ -0,0 +1,10 @@
+public class diffrence_in_print {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            System.out.println("printhing value:"+i);
+    
+            System.out.printf("printhing :%d\n",i);}
+        
+
+    }
+}
---
+++ while_loop.java
@@ -0,0 +1,10 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int Number1=23;
+        int i=0;
+        while(Number1>i){
+            System.out.println(i);
+            i=i+1;
+        }
+    }
+}
