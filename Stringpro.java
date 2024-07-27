044658039b784e778d27965a8ca0ec86913c57d8
VISHNUVARADHAN-A <vishnuviva396@gmail.com>
2024-07-28 02:52:42 +0530

    none

--- Stringpro.java
+++ Stringpro.java
@@ -5,13 +5,18 @@ public class Stringpro {
         //String literal = java will not another objectmemory allocation. because hello is already available on A1
         String A1  ="hello"; 
         String A2 = "hello";
+    
+
         System.out.println(System.identityHashCode(A1));
         System.out.println(System.identityHashCode(A2));
+
         // new object memory even hi is available in A3
         String A3= new String("hi");
         String A4= new String("hi");
+
         System.out.println(System.identityHashCode(A3));
         System.out.println(System.identityHashCode(A4));
+
         String s = "hi hello welcome";
         String[] splittedString =s.split(" ");
         System.out.println(splittedString[0]);
