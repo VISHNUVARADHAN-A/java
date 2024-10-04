5a29795b0b7dfcb353edbdc9fe695324f54b3384
VISHNUVARADHAN-A <vishnuviva396@gmail.com>
2024-10-04 10:41:50 +0530

    initial

---
+++ CreateMethodDemo2.java
@@ -0,0 +1,12 @@
+public class CreateMethodDemo2 {
+    public void getdata(){
+        System.out.println("hello from Demovoid getdata in CreateMethodDemo2");
+    }
+    public void getdata1(){
+        System.out.println("hello from Demovoid getdata1 in CreateMethodDemo2");
+    }
+    public static void DemoStatic1(){
+        System.out.println("hello from Demo static in CreateMethodDemo2");
+    }
+}
+
